02633b74eaf08253f3d9ce61ef2b8519ee8282de
r.bergmann <rene.bergmann@redteclab.com>
2018-05-23 16:20:56 +0200

    ...

--- src/classes/tsmeta.factories/tsmeta.argument.factory.ts
+++ src/classes/tsmeta.factories/tsmeta.argument.factory.ts
@@ -1,6 +1,6 @@
 import { Expression, ObjectLiteralExpression, SyntaxKind } from 'typescript'
 import { ExpressionToString, ObjectLiteralExpressionToString } from '../../lib/ts.methods'
-import { TsArgument, TsType } from '../../lib/tsmeta.schema'
+import { TsArgument } from '../../lib/tsmeta.schema'
 
 /**
  * class TsMetaArgumentFactory
