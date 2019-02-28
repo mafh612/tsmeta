e6f24d0a586e08d70311ee1ab55fd71d4836d929
r.bergmann <rene.bergmann@redteclab.com>
2019-02-28 13:46:33 +0100

    fix class decorators

--- src/lib/annotations.ts
+++ src/lib/annotations.ts
@@ -6,23 +6,27 @@ import { ModelParam, ParameterParam, PropertyParam, ResponseParam } from './inte
 const classFunction: (target: any) => any = (target: any): any => target
 const methodFunction: (target: any, key: string|Symbol) => any = (target: any, key: string|Symbol): any => target[key as string]
 const propertyFunction: (target: any, key: string|Symbol) => any = (target: any, key: string|Symbol): any => target[key as string]
-const parameterFunction: (target: any, key: string|Symbol, index: number) => any = (target: any, key: string|Symbol, index: number): any => target[<string> key][index] // tslint:disable-line
+const parameterFunction: (target: any, key: string|Symbol, index: number) => any
+  = (target: any, key: string|Symbol, index: number): any => target[key as string][index]
 /**
  * class annotations
  */
-const controller: (name: string) => ClassDecorator = classFunction
-const controllerParam: (parameterParam: ParameterParam) => ClassDecorator = classFunction
-const model: (modelParam: ModelParam|any) => ClassDecorator = classFunction
+const controller: (name: string) => ClassDecorator
+  = (name: string): ClassDecorator => classFunction
+const controllerParam: (parameterParam: ParameterParam) => ClassDecorator
+  = (parameterParam: ParameterParam): ClassDecorator => classFunction
+const model: (modelParam: ModelParam|any) => ClassDecorator
+  = (modelParam: ModelParam|any): ClassDecorator => classFunction
 /**
  * method annotations
  */
-const getRequest: (path: string) => MethodDecorator = (path: string): MethodDecorator => methodFunction
-const postRequest: (path: string) => MethodDecorator = (path: string): MethodDecorator => methodFunction
-const putRequest: (path: string) => MethodDecorator = (path: string): MethodDecorator => methodFunction
-const patchRequest: (path: string) => MethodDecorator = (path: string): MethodDecorator => methodFunction
-const deleteRequest: (path: string) => MethodDecorator = (path: string): MethodDecorator => methodFunction
-const headRequest: (path: string) => MethodDecorator = (path: string): MethodDecorator => methodFunction
-const optionsRequest: (path: string) => MethodDecorator = (path: string): MethodDecorator => methodFunction
+const getMapping: (path: string) => MethodDecorator = (path: string): MethodDecorator => methodFunction
+const postMapping: (path: string) => MethodDecorator = (path: string): MethodDecorator => methodFunction
+const putMapping: (path: string) => MethodDecorator = (path: string): MethodDecorator => methodFunction
+const patchMapping: (path: string) => MethodDecorator = (path: string): MethodDecorator => methodFunction
+const deleteMapping: (path: string) => MethodDecorator = (path: string): MethodDecorator => methodFunction
+const headMapping: (path: string) => MethodDecorator = (path: string): MethodDecorator => methodFunction
+const optionsMapping: (path: string) => MethodDecorator = (path: string): MethodDecorator => methodFunction
 const successResponse: (responseParam?: ResponseParam) => MethodDecorator
   = (responseParam?: ResponseParam): MethodDecorator => methodFunction
 const errorResponse: (responseParam?: ResponseParam) => MethodDecorator
@@ -50,13 +54,13 @@ export {
   controller as Controller,
   controllerParam as ControllerParam,
   model as Model,
-  getRequest as GetRequest,
-  postRequest as PostRequest,
-  putRequest as PutRequest,
-  patchRequest as PatchRequest,
-  deleteRequest as DeleteRequest,
-  optionsRequest as OptionsRequest,
-  headRequest as HeadRequest,
+  getMapping as GetMapping,
+  postMapping as PostMapping,
+  putMapping as PutMapping,
+  patchMapping as PatchMapping,
+  deleteMapping as DeleteMapping,
+  optionsMapping as OptionsMapping,
+  headMapping as HeadMapping,
   successResponse as SuccessResponse,
   errorResponse as ErrorResponse,
   deprecated as Deprecated,
