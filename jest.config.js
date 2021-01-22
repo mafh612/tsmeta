35b47eade91535a5042c5ec2cc6effcd085cc8cb
D. Poetzschke <d.poetzschke@redteclab.com>
2021-01-22 13:58:57 +0100

    fix(package): fix test script

--- jest.config.js
+++ jest.config.js
@@ -1,38 +1,24 @@
 module.exports = {
-  "globals": {
-    "ts-jest": {
-      tsConfig: "tsconfig.test.json"
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json'
     }
   },
-  "testEnvironment": "node",
-    "collectCoverage": true,
-    "collectCoverageFrom": [
-      "**/*.ts",
-      "!**/bin.ts",
-      "!**/index.ts",
-      "!**/*.d.ts",
-      "!**/node_modules/**",
-      "!**/vendor/**"
-    ],
-    "coverageThreshold": {
-      "global": {
-        "branches": 100,
-        "functions": 100,
-        "lines": 100,
-        "statements": 100
-      }
-    },
-    "coverageReporters": [
-      "json",
-      "lcov",
-      "text"
-    ],
-    "transform": {
-      "^.+\\.tsx?$": "ts-jest"
-    },
-    "testRegex": "((\\.|/)(test))\\.ts$",
-    "moduleFileExtensions": [
-      "ts",
-      "js"
-    ]
+  testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: ['**/*.ts', '!**/bin.ts', '!**/index.ts', '!**/*.d.ts', '!**/node_modules/**', '!**/vendor/**'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
+  coverageReporters: ['json', 'lcov', 'text'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  testRegex: '((\\.|/)(test))\\.ts$',
+  moduleFileExtensions: ['ts', 'js']
 }
